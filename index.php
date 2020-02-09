6ee8d035f044601b83e3201e0915da7e6b7c933a
RioBoy <boyrio68@gmail.com>
2020-02-09 22:12:04 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <body>
 	
 	<h1>Hello World</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique quos molestias voluptatum aut explicabo ipsam nihil sed fuga dolor magni!</p>
 
 </body>
 </html>
