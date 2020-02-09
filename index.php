5605d84acdab53205ca0edc69ccd9b2264da864d
RioBoy <boyrio68@gmail.com>
2020-02-09 22:17:00 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 	
 	<h1>Hello World</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique quos molestias voluptatum aut explicabo ipsam nihil sed fuga dolor magni!</p>
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
 
 </body>
 </html>
